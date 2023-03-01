c1127d94c07bdb96b634ac958ec815425899721d
Leonardo Maia <leom.mendonca10@gmail.com>
2023-03-01 15:31:38 -0300

    terraform create cluster digital ocean

---
+++ terraform/main.tf
@@ -0,0 +1,17 @@
+resource "digitalocean_kubernetes_cluster" "k8s" {
+  name    = "k8s"
+  region  = var.region
+  version = "1.25.4-do.0"
+  tags    = ["terraform", "k8s"]
+
+  node_pool {
+    name       = "k8s-pool"
+    size       = "s-1vcpu-2gb"
+    node_count = 1
+  }
+}
+
+resource "local_file" "name" {
+  content  = digitalocean_kubernetes_cluster.k8s.kube_config.0.raw_config
+  filename = "kube_config.yaml"
+}
